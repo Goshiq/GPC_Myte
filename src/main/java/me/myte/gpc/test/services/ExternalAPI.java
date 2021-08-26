a6835a3639c0bc1f58de432cd6fa4529e7ae4f37
Goshiq <vangog-2005@ngs.ru>
2021-08-26 21:31:14 +0300

    I need API-key

--- src/main/java/me/myte/gpc/test/services/ExternalAPI.java
+++ src/main/java/me/myte/gpc/test/services/ExternalAPI.java
@@ -1,12 +1,22 @@
 package me.myte.gpc.test.services;
 
 import me.myte.gpc.test.models.Place;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class ExternalAPI {
 
-    private final String url = "https://catalog.api.2gis.com/3.0/items/geocode";
+    // 2gis
+    private static String url = "https://catalog.api.2gis.com/3.0/items/geocode";
+
+    private static WebClient webclient;
+
+    // Here key
+    private static String key = "E-UwPIvFeDSLsyicuaLQ419T4uVQj46ggfVC00wXIs8";
 
     public static Place findByName(String name) {
+        url += ("?q=" + name + "&fields=items.point&key=" + key);
+        webclient = WebClient.create(url);
         return new Place("By name");
     }
 
